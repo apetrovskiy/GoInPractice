edbc7dd137d7273b1c09a9f248cfd336f1637ae0
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2019-12-01 21:27:58 +0300

    the initial commit

---
+++ test1.4.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main3()  {
+	resp, _ := http.Get("http://example.com/")
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	resp.Body.Close()
+}
